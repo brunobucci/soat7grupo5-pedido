862bcfc8641f7696218dbcb4daf70b8c0f05fa5c
marceloquevedo <marceloquevedo@mbp-de-marcelo>
2024-11-24 20:32:37 -0300

    Primeiro commit

---
+++ src/main/java/com/br/fiap/postech/soat7grupo5_pedido/Soat7grupo5PedidoApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soat7grupo5PedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat7grupo5PedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/postech/soat7grupo5_pedido/Soat7grupo5PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Soat7grupo5PedidoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
